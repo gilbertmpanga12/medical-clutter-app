ca30c76906d32eefbd7a7f89b2f0e5d019d712bc
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2021-07-07 12:16:15 +0300

    UPDATE: minor changes to commit

--- lib/screens/screenings.dart
+++ lib/screens/screenings.dart
@@ -14,25 +14,79 @@ class _ScreeningsState extends State<Screenings> {
     const _defaultTextColor = Color(0xff1F2937);
     const _indicatorColor = Color(0xff111827);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.amberAccent,
-        automaticallyImplyLeading: true,
-        iconTheme: IconThemeData(color: _indicatorColor),
-        title: Text(
-          'Screenings',
-          style: GoogleFonts.montserrat(
-            textStyle: TextStyle(
-                color: _defaultTextColor, fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.amberAccent,
+          automaticallyImplyLeading: true,
+          iconTheme: IconThemeData(color: _indicatorColor),
+          title: Text(
+            'Screenings',
+            style: GoogleFonts.montserrat(
+              textStyle: TextStyle(
+                  color: _defaultTextColor, fontWeight: FontWeight.w500),
+            ),
           ),
         ),
-      ),
-      body: SafeArea(
-        child: MedicalImagesScreen(
-          operationType: "screenings",
+        body: SafeArea(
+          child: MedicalImagesScreen(
+            operationType: "screenings",
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      ListTile(
+                        leading: new Icon(Icons.edit),
+                        title: new Text(
+                          'Manually enter records',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17),
+                        ),
+                        subtitle: Text(
+                          'Enables to use an enditor and store records.',
+                        ),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      ListTile(
+                        leading: new Icon(Icons.camera),
+                        title: new Text('Use camera to enter records',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17)),
+                        subtitle: Text(
+                            'Uses our photo recognition AI to store records for you.'),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      SizedBox(
+                        height: 40,
+                      )
+                    ],
+                  );
+                });
+          },
+          label: Text('TAKE PICTURES',
+              style: GoogleFonts.montserrat(
+                textStyle: TextStyle(fontWeight: FontWeight.bold),
+              )),
+          icon: Icon(Icons.camera),
+          backgroundColor: Colors.pink,
+        ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniEndFloat);
   }
 }
